ff8def3f149dfddbc1b1ededa5216961ee8e7ff2
ims <ims21@users.sourceforge.net>
2025-06-08 21:25:13 +0200

    2.21 - added help texts to main menu

--- plugin/ui.py
+++ plugin/ui.py
@@ -4,7 +4,7 @@ from . import _, ngettext
 
 #
 #  Refresh Bouquet - Plugin E2 for OpenPLi
-VERSION = "2.20"
+VERSION = "2.21"
 #  by ims (c) 2016-2025 ims21@users.sourceforge.net
 #
 #  This program is free software; you can redistribute it and/or
@@ -241,14 +241,14 @@ class refreshBouquet(Screen, HelpableScreen):
 		if self.sourceItem or self.targetItem:			# at least one is selected
 			if self.sourceItem and self.targetItem:		# both are selected
 				if self.sourceItem != self.targetItem:	# source != target
-					menu.append((_("Manually replace services"), 0))
-					menu.append((_("Add selected services to target bouquet"), 1))
-					menu.append((_("Add selected missing services to target bouquet"), 2))
-					menu.append((_("Refresh services in target bouquet"), 4))
+					menu.append((_("Manually replace services"), 0, _("Manually replace programs in the target bouquet with programs from the source bouquet.")))
+					menu.append((_("Add selected services to target bouquet"), 1, _("Add one or more selected programs from the source bouquet to the target bouquet at once.")))
+					menu.append((_("Add selected missing services to target bouquet"), 2, _("Only the programs from the source overview that are missing in the target bouquet are displayed. The user can select these programs and transfer them to the target bouquet all at once.")))
+					menu.append((_("Refresh services in target bouquet"), 4, _("Compares the parameters of identical programs in the source and target bouquet, and if differences are found, offers them for selection and replacement with the parameters from the source bouquet.")))
 					buttons += ["blue", " ", "yellow", "green"]
 		if self["config"].getCurrent():
-			menu.append((_("Move selected services in bouquet") + " '%s'" % bName, 5))
-			menu.append((_("Remove selected services in bouquet") + " '%s'" % bName, 3))
+			menu.append((_("Move selected services in bouquet") + " '%s'" % bName, 5, _("Move one service or more selected services in bouquet to new position.")))
+			menu.append((_("Remove selected services in bouquet") + " '%s'" % bName, 3, _("Delete one service or more marked services from bouquet.")))
 			buttons += ["6", "8"]
 		if cfg.rbbfiles.value: # rbb for sources only
 			menu.append((_("Create '%s.rbb' file") % bName, 20))
@@ -256,18 +256,15 @@ class refreshBouquet(Screen, HelpableScreen):
 			if self.isRbbFile():
 				menu.append((_("Create bouquet from rbb file"), 21))
 				buttons += [""]
-		menu.append((_("Create new bouquet"), 13))
+		menu.append((_("Create new bouquet"), 13, _("Creates a new bouquet with the specified name.")))
 		buttons += [""]
 		if self["config"].getCurrent():
-			menu.append((_("Rename bouquet '%s'") % bName, 14))
+			menu.append((_("Rename bouquet '%s'") % bName, 14, _("Rename selected bouquet.")))
 			buttons += ["2"]
-			menu.append((_("Remove bouquet '%s'") % bName, 15))
+			menu.append((_("Remove bouquet '%s'") % bName, 15, _("Delete selected bouquet.")))
 			buttons += [""]
 			if cfg.autozap.value > "0":
-				menu.append((_("Autozap across '%s' bouquet") % bName, 50))
-				buttons += [""]
-		if self.isDeletedBouquet():
-			menu.append((_("Manage deleted bouquets"), 18))
+				menu.append((_("Autozap across '%s' bouquet") % bName, 50, _("Automatically switches between programs in the bouquet at the time interval set in 'Settings...'")))
 			buttons += [""]
 		if cfg.transedit.value and self["config"].getCurrent():
 			name =  self.plainString(self["config"].getCurrent()[0]).decode().replace(' ', '_')
@@ -275,7 +272,7 @@ class refreshBouquet(Screen, HelpableScreen):
 			buttons += [""]
 			menu.append((_("Create TE files from all bouquets to '/tmp'"), 31))
 			buttons += [""]
-		menu.append((_("Settings..."), 10))
+		menu.append((_("Settings..."), 10, _("Setting various plugin options")))
 		buttons += ["menu"]
 		self.session.openWithCallback(self.menuCallback, ChoiceBox, title=_("Select action for bouquet:"), list=menu, keys=["dummy" if key=="" else key for key in buttons])
 		self["info"].setText(self.infotext)
